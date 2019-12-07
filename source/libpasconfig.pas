be4f4b74feeacd9260d1e0d6d3ed116c9a9e9a3a
isemenkov <ivan@semenkov.pro>
2019-12-07 11:47:41 +0200

    Update libpasconfig.pas

--- source/libpasconfig.pas
+++ source/libpasconfig.pas
@@ -105,9 +105,9 @@ type
 
   pconfig_t = ^config_t;
 
-  ppconfig_settings_t = ^pconfig_settings_t;
-  pconfig_settings_t = ^config_settings_t;
-  config_settings_t = record
+  ppconfig_setting_t = ^pconfig_setting_t;
+  pconfig_setting_t = ^config_setting_t;
+  config_setting_t = record
     name : PChar;
     settings_type : Smallint;
     format : Smallint;
@@ -308,6 +308,214 @@ procedure config_set_include_func (config : pconfig_t; func :
   config_include_fn_t); cdecl; external libConfig;
 {$ENDIF}
 
+{$IFDEF LIBCONFIG_VER_MAJOR    >= 1 AND
+        LIBCONFIG_VER_MINOR    >= 7 AND
+        LIBCONFIG_VER_REVISION >= 0}
+{ These functions get and set the number of decimal digits to output after the
+  radix character when writing the configuration to a file or stream.
+
+  Valid values for digits range from 0 (no decimals) to about 15 (implementation
+  defined). This parameter has no effect on parsing.
+
+  The default float precision is 6. }
+function config_get_float_precision (const config : pconfig_t) : Word; cdecl;
+  external libConfig;
+procedure config_set_float_precision (config : pconfig_t; digits : Word); cdecl;
+  external libConfig;
+{$ENDIF}
+
+{ These functions get and set the options for the configuration config. The
+  options affect how configurations are read and written. The following options
+  are defined:
+
+    CONFIG_OPTION_AUTOCONVERT
+        Turning this option on enables number auto-conversion for the
+        configuration. When this feature is enabled, an attempt to retrieve a
+        floating point setting’s value into an integer (or vice versa), or store
+        an integer to a floating point setting’s value (or vice versa) will
+        cause the library to silently perform the necessary conversion (possibly
+        leading to loss of data), rather than reporting failure. By default this
+        option is turned off.
+
+    CONFIG_OPTION_SEMICOLON_SEPARATORS
+        This option controls whether a semicolon (‘;’) is output after each
+        setting when the configuration is written to a file or stream. (The
+        semicolon separators are optional in the configuration syntax.) By
+        default this option is turned on.
+
+    CONFIG_OPTION_COLON_ASSIGNMENT_FOR_GROUPS
+        This option controls whether a colon (‘:’) is output between each group
+        setting’s name and its value when the configuration is written to a file
+        or stream. If the option is turned off, an equals sign (‘=’) is output
+        instead. (These tokens are interchangeable in the configuration syntax.)
+        By default this option is turned on.
+
+    CONFIG_OPTION_COLON_ASSIGNMENT_FOR_NON_GROUPS
+        This option controls whether a colon (‘:’) is output between each
+        non-group setting’s name and its value when the configuration is written
+        to a file or stream. If the option is turned off, an equals sign (‘=’)
+        is output instead. (These tokens are interchangeable in the
+        configuration syntax.) By default this option is turned off.
+
+    CONFIG_OPTION_OPEN_BRACE_ON_SEPARATE_LINE
+        This option controls whether an open brace (‘{’) will be written on its
+        own line when the configuration is written to a file or stream. If the
+        option is turned off, the brace will be written at the end of the
+        previous line. By default this option is turned on.
+
+    CONFIG_OPTION_ALLOW_SCIENTIFIC_NOTATION
+        (Since v1.7) This option controls whether scientific notation may be
+        used as appropriate when writing floating point values (corresponding
+        to printf() ‘%g’ format) or should never be used (corresponding to
+        printf() ‘%f’ format). By default this option is turned off.
+
+    CONFIG_OPTION_FSYNC
+        (Since v1.7.1) This option controls whether the config_write_file()
+        function performs an fsync operation after writing the configuration and
+        before closing the file. By default this option is turned off. }}
+function config_get_options (const config : pconfig_t) : Integer; cdecl;
+  external libConfig;
+procedure config_set_options (config : pconfig_t; options : Integer); cdecl;
+  external libConfig;
+
+{$IFDEF LIBCONFIG_VER_MAJOR    >= 1 AND
+        LIBCONFIG_VER_MINOR    >= 7 AND
+        LIBCONFIG_VER_REVISION >= 0}
+{ These functions get and set the given option of the configuration config. The
+  option is enabled if flag is CONFIG_TRUE and disabled if it is CONFIG_FALSE.
+
+  See config_set_options() above for the list of available options. }
+function config_get_option (const config : pconfig_t; option : Integer) :
+  Integer; cdecl; external libConfig;
+procedure config_set_option (config : pconfig_t; option : Integer; flag :
+  Integer); cdecl; external libConfig;
+{$ENDIF}
+
+{$IFDEF LIBCONFIG_VER_MAJOR    <= 1 AND
+        LIBCONFIG_VER_MINOR    <= 5 AND
+        LIBCONFIG_VER_REVISION <= 0}
+{ These functions get and set the CONFIG_OPTION_AUTO_CONVERT option. They are
+  obsoleted by the config_set_option() and config_get_option() functions
+  described above. }
+function config_get_auto_convert (const config : pconfig_t) : Integer; cdecl;
+  external libConfig;
+procedure config_set_auto_convert (config : pconfig_t; flag : Integer); cdecl;
+  external libConfig;
+{$ENDIF}
+{$IFDEF LIBCONFIG_VER_MAJOR    >= 1 AND
+        LIBCONFIG_VER_MINOR    >= 7 AND
+        LIBCONFIG_VER_REVISION >= 0}
+{ These functions get and set the CONFIG_OPTION_AUTO_CONVERT option. They are
+  obsoleted by the config_set_option() and config_get_option() functions
+  described above. }
+function config_get_auto_convert (const config : pconfig_t) : Integer; inline;
+procedure config_set_auto_convert (config : pconfig_t; flag : Integer); inline;
+{$ENDIF}
+
+{ These functions, which are implemented as macros, get and set the default
+  external format for settings in the configuration config. If a non-default
+  format has not been set for a setting with config_setting_set_format(), this
+  configuration-wide default format will be used instead when that setting is
+  written to a file or stream.}
+function config_get_default_format (config : pconfig_t) : Word; inline;
+procedure config_set_default_format (config : pconfig_t; format : Word); inline;
+
+{ These functions, which are implemented as macros, get and set the tab width
+  for the configuration config. The tab width affects the formatting of the
+  configuration when it is written to a file or stream: each level of nesting is
+  indented by width spaces, or by a single tab character if width is 0. The tab
+  width has no effect on parsing.
+
+  Valid tab widths range from 0 to 15. The default tab width is 2. }
+function config_get_tab_width (const config : pconfig_t) : Word; inline;
+procedure config_set_tab_Width (config : pconfig_t; width : Word); inline;
+
+{ These functions look up the value of the setting in the configuration config
+  specified by the path path. They store the value of the setting at value and
+  return CONFIG_TRUE on success. If the setting was not found or if the type of
+  the value did not match the type requested, they leave the data pointed to by
+  value unmodified and return CONFIG_FALSE.
+
+  Storage for the string returned by config_lookup_string() is managed by the
+  library and released automatically when the setting is destroyed or when the
+  setting’s value is changed; the string must not be freed by the caller. }
+function config_lookup_int (const config : pconfig_t; const path : PChar;
+  value : PInteger) : Integer; cdecl; external libConfig;
+function config_lookup_int64 (const config : pconfig_t; const path : PChar;
+  value : PInt64) : Integer; cdecl; external libConfig;
+function config_lookup_float (const config : pconfig_t; const path : PChar;
+  value : PDouble) : Integer; cdecl; external libConfig;
+function config_lookup_bool (const config : pconfig_t; const path : PChar;
+  value : PInteger) : Integer; cdecl; external libConfig;
+function config_lookup_string (const config : pconfig_t; const path : PChar;
+  value : PPChar) : Integer; cdecl; external libConfig;
+
+{ This function locates the setting in the configuration config specified by the
+  path path. It returns a pointer to the config_setting_t structure on success,
+  or NULL if the setting was not found. }
+function config_lookup (const config : pconfig_t; const path : PChar) :
+  pconfig_setting_t; cdecl; external libConfig;
+
+{ This function locates a setting by a path path relative to the setting
+  setting. It returns a pointer to the config_setting_t structure on success, or
+  NULL if the setting was not found. }
+function config_setting_lookup (setting : pconfig_setting_t; const path :
+  PChar) : pconfig_setting_t; cdecl; external libConfig;
+
+{ These functions return the value of the given setting. If the type of the
+  setting does not match the type requested, a 0 or NULL value is returned.
+  Storage for the string returned by config_setting_get_string() is managed by
+  the library and released automatically when the setting is destroyed or when
+  the setting’s value is changed; the string must not be freed by the caller. }
+function config_setting_get_int (const setting : pconfig_setting_t) : Integer;
+  cdecl; external libConfig;
+function config_setting_get_int64 (const setting : pconfig_setting_t) :
+  Int64; cdecl; external libConfig;
+function config_setting_get_float (const setting : pconfig_setting_t) :
+  Double; cdecl; external libConfig;
+function config_setting_get_bool (const setting : pconfig_setting_t) :
+  Integer; cdecl; external libConfig;
+function config_setting_get_string (const setting : pconfig_setting_t) :
+  PChar; cdecl; external libConfig;
+
+{ These functions set the value of the given setting to value. On success, they
+  return CONFIG_TRUE. If the setting does not match the type of the value, they
+  return CONFIG_FALSE. config_setting_set_string() makes a copy of the passed
+  string value, so it may be subsequently freed or modified by the caller
+  without affecting the value of the setting. }
+function config_setting_set_int (setting : pconfig_setting_t; value : Integer) :
+  Integer; cdecl; external libConfig;
+function config_setting_set_int64 (setting : pconfig_setting_t; value : Int64) :
+  Integer; cdecl; external libConfig;
+function config_setting_set_float (setting : pconfig_setting_t; value : Double)
+  : Integer; cdecl; external libConfig;
+function config_setting_set_bool (setting : pconfig_setting_t; value : Integer)
+  : Integer; cdecl; external libConfig;
+function config_setting_set_string (setting : pconfig_setting_t; const value :
+  PChar) : Integer; cdecl; external libConfig;
+
+{ These functions look up the value of the child setting named name of the
+  setting setting. They store the value at value and return CONFIG_TRUE on
+  success. If the setting was not found or if the type of the value did not
+  match the type requested, they leave the data pointed to by value unmodified
+  and return CONFIG_FALSE.
+
+  Storage for the string returned by config_setting_lookup_string() is managed
+  by the library and released automatically when the setting is destroyed or
+  when the setting’s value is changed; the string must not be freed by the
+  caller. }
+function config_setting_lookup_int (const setting : pconfig_setting_t; const
+  name : PChar; value : PInteger) : Integer; cdecl; external libConfig;
+function config_setting_lookup_int64 (const setting : pconfig_setting_t; const
+  name : PChar; value : PInt64) : Integer; cdecl; external libConfig;
+function config_setting_lookup_float (const setting : pconfig_setting_t; const
+  name : PChar; value : PDouble) : Integer; cdecl; external libConfig;
+function config_setting_lookup_bool (const setting : pconfig_setting_t; const
+  name : PChar; value : PInteger) : Integer; cdecl; external libConfig;
+function config_setting_lookup_string (const setting : pconfig_setting_t; const
+  name : PChar; value : PPChar); cdecl; external libConfig;
+
+
 
 
 implementation
@@ -336,6 +544,42 @@ function config_get_include_dir(const config: pconfig_t): PChar;
 begin
   Result := config^.include_dir;
 end;
+
+{$IFDEF LIBCONFIG_VER_MAJOR    >= 1 AND
+        LIBCONFIG_VER_MINOR    >= 7 AND
+        LIBCONFIG_VER_REVISION >= 0}
+function config_get_auto_convert(const config: pconfig_t): Integer;
+begin
+  Result := config_get_option(config, CONFIG_OPTION_AUTOCONVERT);
+end;
+
+procedure config_set_auto_convert(config: pconfig_t; flag: Integer);
+begin
+  config_set_option(config, CONFIG_OPTION_AUTOCONVERT, flag);
+end;
+{$ENDIF}
+
+function config_get_default_format(config: pconfig_t): Word;
+begin
+  Result := config^.default_format;
+end;
+
+procedure config_set_default_format(config: pconfig_t; format: Word);
+begin
+  config^.default_format := format;
+end;
+
+function config_get_tab_width(const config: pconfig_t): Word;
+begin
+  Result := config^.tab_width;
+end;
+
+procedure config_set_tab_Width(config: pconfig_t; width: Word);
+begin
+  config^.tab_width := width;
+end;
+
+
 
 end.
 
